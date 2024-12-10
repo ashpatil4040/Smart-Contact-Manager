3a506a460652eb0a0068cb92219e8ad39afcae1d
akash <ashpatil4040@gmail.com>
2024-12-09 23:24:50 -0600

    Initial commit

---
+++ src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,35 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/home")
+    public String Home(Model model) {
+        System.out.println("Home Page");
+
+        // send data to home.html
+        model.addAttribute("name", "Substring Technologies");
+        model.addAttribute("youtubeChannel", "https://www.youtube.com/channel/UC785368v212041307");
+        model.addAttribute("address", "India");
+        return "home";
+    }
+
+    // about page
+
+    @RequestMapping("/about")
+    public String aboutPage(Model model) {
+        System.out.println("About Page");
+        model.addAttribute("isLogin", false);;
+        return "about";
+    }
+    // services page
+    @RequestMapping("/services")
+    public String servicesPage(Model model) {
+        System.out.println("Services Page");
+        return "services";
+    }
+}
