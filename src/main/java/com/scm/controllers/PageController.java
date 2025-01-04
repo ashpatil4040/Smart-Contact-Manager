6db70e981b7a3e3381f51e72c3c0e088ccd73f25
akash <ashpatil4040@gmail.com>
2025-01-04 02:20:15 -0600

    Implement user registration functionality with User entity, service, and form; add ResourceNotFoundException

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,13 +1,23 @@
 package com.scm.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.scm.entities.User;
+import com.scm.forms.UserFrom;
+import com.scm.services.UserService;
 
 @Controller
 public class PageController {
-
+    
+    @Autowired
+    private UserService userService;
+    
     @RequestMapping("/home")
     public String Home(Model model) {
         System.out.println("Home Page");
@@ -47,8 +57,36 @@ public class PageController {
     }
     
     @GetMapping("/register")
-    public String register() {
+    public String register(Model model) {
+        
+        UserFrom userForm = new UserFrom();
+        // set default value
+        // userForm.setName("John Doe");
+        // userForm.setEmail("john@gmail.com");
+        // userForm.setPassword("123456");
+        // userForm.setAbout("I am a software developer");
+        // userForm.setPhoneNumber("1234567890");
+        model.addAttribute("userForm", userForm);
+        
         return "register";
     }
     
+    //processing register form
+    @PostMapping("/do-register")
+    public String processRegister(@ModelAttribute UserFrom userForm) {
+         
+        // UserFrom--->User
+        User user = User.builder()
+                        .name(userForm.getName())
+                        .email(userForm.getEmail())
+                        .password(userForm.getPassword())
+                        .about(userForm.getAbout())
+                        .phoneNumber(userForm.getPhoneNumber())
+                        .profilePic("src\\main\\resources\\static\\images\\default_ProfilePic.png")
+                        .build();
+
+        User savedUser = userService.saveUser(user);
+        
+        return "redirect:/register";
+    } 
 }
---
+++ src/main/java/com/scm/services/UserService.java
@@ -0,0 +1,18 @@
+package com.scm.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.scm.entities.User;
+import com.scm.forms.UserFrom;
+
+public interface UserService {
+
+    User saveUser(User user);
+    Optional<User> getUserById(String id);
+    Optional<User> updateUser(User user);
+    void deleteUser(String id);
+    boolean isUserExistByEmail(String email);
+    boolean isUserExist(String userId);
+    List<User> getAllUsers();
+}
