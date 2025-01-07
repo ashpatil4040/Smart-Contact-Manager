41b405bca19e758b3d3f706d7eab06d650cd4ffe
akash <ashpatil4040@gmail.com>
2025-01-07 00:50:13 -0600

    Enhance login functionality with custom form layout and error handling; add logout link to user dashboard

--- src/main/java/com/scm/config/SecurityConfig.java
+++ src/main/java/com/scm/config/SecurityConfig.java
@@ -3,14 +3,12 @@ package com.scm.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
 import com.scm.services.impl.SecurityCustomUserDetailService;
 
 @Configuration
@@ -55,16 +53,60 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-        // configure the http security
-        // url based authorization konse public hai ya private hai
+        // configuration
+
+        // urls configure kiay hai ki koun se public rangenge aur koun se private
+        // rangenge
         httpSecurity.authorizeHttpRequests(authorize -> {
-        // authorize.requestMatchers("/home","register").permitAll();
-         authorize.requestMatchers("/user/**").authenticated();
-         authorize.anyRequest().permitAll();
-        } 
+            // authorize.requestMatchers("/home", "/register", "/services").permitAll();
+            authorize.requestMatchers("/user/**").authenticated();
+            authorize.anyRequest().permitAll();
+        });
+
+        // form default login
+        // agar hame kuch bhi change karna hua to hama yaha ayenge: form login se
+        // related
+        httpSecurity.formLogin(formLogin -> {
+
+            formLogin.loginPage("/login")
+                      .loginProcessingUrl("/authenticate")
+                      .defaultSuccessUrl("/user/dashboard", true)
+                      .failureUrl("/login?error=true")
+            // formLogin.defaultSuccessUrl("/home");
+                      .usernameParameter("email")
+                      .passwordParameter("password");
+
+            
+            // formLogin.failureHandler(new AuthenticationFailureHandler() {
+                
+            //     @Override
+            //     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+            //             AuthenticationException exception) throws IOException, ServletException {
+            //         // TODO Auto-generated method stub
+            //         throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationFailure'");
+            //     }
+            // });
+
+            // formLogin.successHandler(new AuthenticationSuccessHandler() {
+
+            //     @Override
+            //     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            //             Authentication authentication) throws IOException, ServletException {
+            //         // TODO Auto-generated method stub
+            //         throw new UnsupportedOperationException("Unimplemented method 'onAuthenticationSuccess'");
+            //     }
+                
+            // });
+        }
+
         );
-        //form login
-        httpSecurity.formLogin(Customizer.withDefaults());
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.logout(logoutForm -> {
+            logoutForm.logoutUrl("/do-logout");
+            logoutForm.logoutSuccessUrl("/login?logout=true");
+        });
+
         return httpSecurity.build();
     }
         
