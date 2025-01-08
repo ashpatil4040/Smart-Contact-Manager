c85f3f023c3082df542198fcffb1543e116c5856
akash <ashpatil4040@gmail.com>
2025-01-08 00:56:32 -0600

    Implement OAuth2 login for Google and GitHub; enhance user profile handling and update login UI

--- src/main/java/com/scm/config/OAuthAuthenticationSuccessHandler.java
+++ src/main/java/com/scm/config/OAuthAuthenticationSuccessHandler.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -35,8 +36,66 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
             Authentication authentication) throws IOException, ServletException {
         
         logger.info("OAuthAuthenticationSuccessHandler.onAuthenticationSuccess() called");
+ 
+        // identtify the provider
 
-        DefaultOAuth2User user = (DefaultOAuth2User)authentication.getPrincipal();
+       var oauth2AuthenticationToken=  (OAuth2AuthenticationToken)authentication;
+
+         String authorizedClientRegistrationId = oauth2AuthenticationToken.getAuthorizedClientRegistrationId();
+         
+         logger.info(authorizedClientRegistrationId);
+
+        var oauthUser= (DefaultOAuth2User)authentication.getPrincipal();
+
+        oauthUser.getAttributes().forEach((k,v)->{
+            logger.info(k + " : " + v);
+        });
+        
+        User user = new User();
+        user.setUserId(UUID.randomUUID().toString());
+        user.setRoleList(List.of(AppConstants.ROLE_USER));
+        user.setEmailVerified(true); 
+        user.setEnabled(true);
+        user.setPassword("dummy");
+
+        if(authorizedClientRegistrationId.equalsIgnoreCase("GOOGLE")){
+                 
+          //google user
+          //google attributes
+          user.setName(oauthUser.getAttribute("name").toString());
+          user.setProfilePic(oauthUser.getAttribute("picture").toString());
+          user.setEmail(oauthUser.getAttribute("email").toString());
+          user.setProviderUserId(oauthUser.getName());
+          user.setProvider(Providers.GOOGLE);
+          user.setAbout("This account is created by using Google");
+
+
+        }
+        else if(authorizedClientRegistrationId.equalsIgnoreCase("GITHUB")){
+        //github user
+        //github attributes
+        
+          String email = oauthUser.getAttribute("email") != null ? oauthUser.getAttribute("email").toString() : oauthUser.getAttribute("login").toString()+"@gmail.com";
+          
+          String picture = oauthUser.getAttribute("avatar_url").toString();
+          String name = oauthUser.getAttribute("login").toString();
+          String providerUserId = oauthUser.getName();
+
+         user.setEmail(email);
+         user.setProfilePic(picture);
+         user.setName(name);
+         user.setProvider(Providers.GITHUB);
+         user.setProviderUserId(providerUserId);
+         user.setAbout("This account is created by using Github");
+        }
+
+        //facebook user
+        //facebook attributes
+
+
+        
+        /* 
+         DefaultOAuth2User user = (DefaultOAuth2User)authentication.getPrincipal();
     
         // logger.info(user.getName());
 
@@ -72,6 +131,15 @@ public class OAuthAuthenticationSuccessHandler implements AuthenticationSuccessH
             userRepo.save(user1);
             logger.info("User saved" + email);
         }
+        */     
+        
+        User user2= userRepo.findByEmail(user.getEmail()).orElse( null);
+        
+        if(user2 == null) {
+            userRepo.save(user);
+            logger.info("User saved" +  user.getEmail());
+        }
+        
          new DefaultRedirectStrategy().sendRedirect(request, response, "/user/profile");
     }
 
